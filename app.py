7759e3cadcf7a2e79c8ce5ec49e952c9ca698add
Rami <ramifareed1989@hotmail.com>
2020-02-22 21:38:47 +0000

    added add recipe/ categories & home

--- app.py
+++ app.py
@@ -2,18 +2,42 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'the_cook_book'
-app.config["MONGO_URI"] = 'mongodb+srv://RamiSWE:Safa1992@myfirstcluster-guo1h.mongodb.net/the_cook_book?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    _recipes=mongo.db.recipes.find()
+    recipe_list = [recipe for recipe in _recipes ]
+    return render_template("recipes_list.html", recipes=recipe_list )
+
+@app.route('/add_recipes')
+def add_recipes():
+    _categories=mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("addrecipe.html", categories = category_list)
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    _categories=mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("categories.html", categories = category_list)
 
 
 if __name__ == '__main__':
