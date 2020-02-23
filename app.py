cf33a8551ad5941d70b80e41b705695aaeb1ab39
Rami <ramifareed1989@hotmail.com>
2020-02-23 19:25:49 +0000

    added functions like edit task & delete task

--- app.py
+++ app.py
@@ -32,6 +32,32 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update(
+        {'_id': ObjectId(recipe_id)},
+        {
+            'recipe_name': request.form.get('recipe_name'),
+            'category_name': request.form.get('category_name'),
+            'recipe_description': request.form.get('recipe_description'),
+            'recipe_ingredients': request.form.get('recipe_ingredients'),
+            'recipe_directions': request.form.get('recipe_directions'),
+            'recipe_image': request.form.get('recipe_image')
+        })
+    return redirect(url_for('get_recipes'))
+
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
 
 @app.route('/get_categories')
 def get_categories():
